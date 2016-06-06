e4f55c6932e88d709a6e80473e30541fc9061deb
edhemphill <edhemphill@gmail.com>
2016-06-06 11:15:09 -0500

    added docs

---
+++ docs/api.js
@@ -0,0 +1,18 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "deviceJSUtils",
+        "orderedTable"
+    ],
+    "modules": [
+        "utils"
+    ],
+    "allModules": [
+        {
+            "displayName": "utils",
+            "name": "utils"
+        }
+    ],
+    "elements": []
+} };
+});
